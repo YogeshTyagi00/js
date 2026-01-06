1db14eed2288852fb55fce099614daa29ad4aba6
YogeshTyagi00 <tyagiyogesh848@gmail.com>
2026-01-06 13:10:04 +0000

    string done

---
+++ Basics/string.js
@@ -0,0 +1,18 @@
+const name = "rishi-sharma"
+
+// console.log(name.__proto__)
+
+// console.log(name[0])
+
+// console.log(name.length)
+
+// console.log(name.slice(0,2)) //ri
+// console.log(name.slice(-12,-2)) //rish-shar
+// console.log(name.slice(-1,-12)) 
+
+// console.log(name.substring(0,4))
+
+// console.log(name.replace('-',' ')) //rishi sharma
+
+// console.log(name.trim())
+
